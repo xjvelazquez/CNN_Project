ff00f35c4b7baa5b29f9b623d146b6d0f4908c1c
j5velazq <j5velazq@j5velazq-8234.pods.j5velazq.svc.cluster.local>
2019-11-08 17:56:33 -0800

    Start of our cnn

---
+++ our_cnn.py
@@ -0,0 +1,80 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+from torch.utils.data.sampler import SubsetRandomSampler
+import numpy as np
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as func
+import torch.nn.init as torch_init
+import torch.optim as optim
+import pandas as pd
+import torchvision
+from torchvision import transforms, utils
+import os
+
+class loader(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.transform = transform
+        self.frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+
+    def __len__(self):
+        return len(self.frame)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir,self.frame.iloc[idx, 0])
+        img = Image.open(img_name)
+        label = self.frame.iloc[idx, 1]
+        
+        if self.transform:
+            img = self.transform(img)
+
+        return img, label
+    
+    
+class Nnet(nn.Module):
+    def __init__(self):
+        super(Nnet, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(3, 21 , 3, stride=2, padding=1, bias=False), 
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.Conv2d(21, 20, 3,stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=20, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.Conv2d(20, 15, 3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=15, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            nn.ReLU( inplace=True),
+            nn.Conv2d(15, 7, 5, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=7, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            nn.ReLU( inplace=True),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(1183,500),
+            nn.ReLU( inplace=True),
+            nn.Linear(500, 201),
+            #nn.Softmax()
+        )
+
+    def num_flat_features(self, inputs):
+        
+        # Get the dimensions of the layers excluding the inputs
+        size = inputs.size()[1:]
+        # Track the number of features
+        num_features = 1
+        
+        for s in size:
+            num_features *= s
+        
+        return num_features
+
+    def forward(self, input):
+        x=self.main(input)
+        #print('yo:', self.num_flat_features(x))
+        x=x.view(-1, self.num_flat_features(x))
+        
+        return self.fc(x)
+
+        
